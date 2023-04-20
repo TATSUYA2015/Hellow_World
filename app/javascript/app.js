b9cc70296d0c35a2f170691f7d4a0d059c435ae0
EC2 Default User <git>
2023-04-20 22:42:36 +0000

    js編集

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -6,13 +6,22 @@
 // let str2 = 'World!!';
 // alert(str1 + str2);
 
-let max = 100;
-let num = 1;
-let count = 0;
+// let max = 100;
+// let num = 1;
+// let count = 0;
 
-while(num < max){
-  num = num * 2;
-  count = count + 1;
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
 }
 
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+alert('1から10まで足し算した結果は' + num + 'です');
